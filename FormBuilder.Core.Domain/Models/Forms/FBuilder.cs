d1ed1dd41cfe068ebd80ba153958a9681e3a3dc3
DESKTOP-S47O7JN\ivanmds <ivanmds2@hotmail.com>
2019-04-05 00:00:31 -0300

    refatorando para abstração

--- FormBuilder.Core.Domain/Models/Forms/FBuilder.cs
+++ FormBuilder.Core.Domain/Models/Forms/FBuilder.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using FluentValidation.Results;
-using FormBuilder.Core.Domain.Interfaces.Validation;
-using FormBuilder.Core.Domain.Models.Fields;
 using System.Collections.Generic;
 using FormBuilder.Core.Domain.Validations.Forms;
-using FormBuilder.Core.Domain.Models.Fields.Numbers;
 
 namespace FormBuilder.Core.Domain.Models.Forms
 {
-    public class FBuilder : IValidable
+    public class FBuilder : FBuilderBase
     {
         public FBuilder(string name, DateTime expiredIn)
         {
@@ -20,19 +17,7 @@ namespace FormBuilder.Core.Domain.Models.Forms
         public DateTime ExpiredIn { get; private set; }
         public bool IsActive => ExpiredIn > DateTime.Now;
 
-        private Dictionary<int, BaseField> _fields = new Dictionary<int, BaseField>();
-        public IReadOnlyDictionary<int, BaseField> Fields => _fields;
-
-        public bool AddField(int index, BaseField field)
-        {
-            if (_fields.ContainsKey(index)) return false;
-            if (field == null) throw new NullReferenceException("field not null.");
-
-            _fields.Add(index, field);
-            return true;
-        }
-
-        public ValidationResult Validate()
+        public override ValidationResult Validate()
         {
             ValidationResult fBuilderValidation = new FBuilderValidation().Validate(this);
             List<ValidationFailure> fieldValidations = GetFieldsValidate();
@@ -41,22 +26,6 @@ namespace FormBuilder.Core.Domain.Models.Forms
                 fieldValidations.AddRange(fBuilderValidation.Errors);
 
             return new ValidationResult(fieldValidations);
-        }
-
-        private List<ValidationFailure> GetFieldsValidate()
-        {
-            List<ValidationFailure> failures = new List<ValidationFailure>();
-            if (Fields.Count > 0)
-            {
-                foreach (var keyValue in Fields)
-                {
-                    ValidationResult result = keyValue.Value.Validate();
-                    if (!result.IsValid)
-                        failures.AddRange(result.Errors);
-                }
-            }
-
-            return failures;
         }
     }
 }
---
+++ FormBuilder.Core.Domain/Models/Forms/FBuilderBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using FluentValidation.Results;
+using FormBuilder.Core.Domain.Interfaces.Validation;
+using FormBuilder.Core.Domain.Models.Fields;
+using System.Collections.Generic;
+
+namespace FormBuilder.Core.Domain.Models.Forms
+{
+    public abstract class FBuilderBase : IValidable
+    {
+        private Dictionary<int, BaseField> _fields = new Dictionary<int, BaseField>();
+        public IReadOnlyDictionary<int, BaseField> Fields => _fields;
+
+        public bool AddField(int index, BaseField field)
+        {
+            if (_fields.ContainsKey(index)) return false;
+            if (field == null) throw new NullReferenceException("field not null.");
+
+            _fields.Add(index, field);
+            return true;
+        }
+        public abstract ValidationResult Validate();
+
+        protected List<ValidationFailure> GetFieldsValidate()
+        {
+            List<ValidationFailure> failures = new List<ValidationFailure>();
+            if (Fields.Count > 0)
+            {
+                foreach (var keyValue in Fields)
+                {
+                    ValidationResult result = keyValue.Value.Validate();
+                    if (!result.IsValid)
+                        failures.AddRange(result.Errors);
+                }
+            }
+
+            return failures;
+        }
+    }
+}
