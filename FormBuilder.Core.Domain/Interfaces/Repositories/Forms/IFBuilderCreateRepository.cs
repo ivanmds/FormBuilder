c48dd38317ec9630db0def9fc1c2548ef762460a
DESKTOP-S47O7JN\ivanmds <ivanmds2@hotmail.com>
2019-04-05 01:03:15 -0300

    ajuste no nome do formulário

--- FormBuilder.Core.Domain/Interfaces/Repositories/Forms/IFBuilderCreateRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using FormBuilder.Core.Domain.Models.Forms;
-
-namespace FormBuilder.Core.Domain.Interfaces.Repositories.Forms
-{
-    public interface IFBuilderCreateRepository
-    {
-        void Add(FormBuild fBuilder);
-    }
-}
