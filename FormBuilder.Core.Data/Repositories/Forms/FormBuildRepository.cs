8987e5b3d111df5f2e37d84fd50044762e28de56
DESKTOP-S47O7JN\ivanmds <ivanmds2@hotmail.com>
2019-04-06 23:12:40 -0300

    criação repositorios

---
+++ FormBuilder.Core.Data/Repositories/Forms/FormBuildRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using FormBuilder.Core.Domain.Interfaces.Repositories.Forms;
+using FormBuilder.Core.Domain.Models.Forms;
+
+namespace FormBuilder.Core.Data.Repositories.Forms
+{
+    public class FormBuildRepository : IFormBuildRepository
+    {
+        public Task AddAsync(FormBuild fBuilder)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<FormBuild> GetAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateAsync(FormBuild fBuilder)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ FormBuilder.Core.Data/Repositories/Forms/FormResponseRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using FormBuilder.Core.Domain.Interfaces.Repositories.Forms;
+using FormBuilder.Core.Domain.Models.Forms.Response;
+
+namespace FormBuilder.Core.Data.Repositories.Forms
+{
+    public class FormResponseRepository : IFormResponseRepository
+    {
+        public Task AddAsync(FormResponse formResponse)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<FormResponse> GetAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateAsync(FormResponse formResponse)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
