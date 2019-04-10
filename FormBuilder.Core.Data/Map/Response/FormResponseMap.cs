bca3737efaf7a0282fcdbfffc35f06550189da4b
DESKTOP-S47O7JN\ivanmds <ivanmds2@hotmail.com>
2019-04-09 23:08:03 -0300

    troca de local

--- FormBuilder.Core.Data/Map/Response/FormResponseMap.cs
+++ FormBuilder.Core.Data/Map/Response/FormResponseMap.cs
@@ -1,14 +1,15 @@
-﻿using FormBuilder.Core.Domain.Models.Forms.Response;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using FormBuilder.Core.Domain.Models.Forms.Response;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace FormBuilder.Core.Data.Map
+namespace FormBuilder.Core.Data.Map.Response
 {
     public class FormResponseMap : IEntityTypeConfiguration<FormResponse>
     {
         public void Configure(EntityTypeBuilder<FormResponse> builder)
         {
             builder.ToTable("FormResponse");
+            builder.HasKey(p => p.Id);
         }
     }
 }
