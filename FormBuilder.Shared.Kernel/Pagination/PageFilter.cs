9f1b3bc9f3bc4e5be540e07487e003747683c185
DESKTOP-S47O7JN\ivanmds <ivanmds2@hotmail.com>
2019-05-07 01:03:54 -0300

    criação do filtro da pagina

---
+++ FormBuilder.Shared.Kernel/Pagination/PageFilter.cs
@@ -0,0 +1,14 @@
+﻿namespace FormBuilder.Shared.Kernel.Pagination
+{
+    public class PageFilter
+    {
+        public PageFilter(int number, int size)
+        {
+            Number = number;
+            Size = size;
+        }
+
+        public int Number { get; protected set; }
+        public int Size { get; protected set; }
+    }
+}
