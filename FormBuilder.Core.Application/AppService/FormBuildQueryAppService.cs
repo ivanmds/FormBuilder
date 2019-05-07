174e4d14c58fea651e4cd27250c8ea0ad69549e4
DESKTOP-S47O7JN\ivanmds <ivanmds2@hotmail.com>
2019-05-07 01:05:03 -0300

    ajustes para paginar

--- FormBuilder.Core.Application/AppService/FormBuildQueryAppService.cs
+++ FormBuilder.Core.Application/AppService/FormBuildQueryAppService.cs
@@ -3,9 +3,10 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using FormBuilder.Core.Data.DbContexts;
-using FormBuilder.Core.Domain.Models.Forms;
 using FormBuilder.Core.Application.AppService.Interfaces;
 using FormBuilder.Core.Application.ViewModels.Forms.Builder;
+using FormBuilder.Shared.Kernel.Pagination;
+using System.Linq;
 
 namespace FormBuilder.Core.Application.AppService
 {
@@ -17,10 +18,16 @@ namespace FormBuilder.Core.Application.AppService
             _dbContext = dbContext;
         }
 
-        public async Task<IEnumerable<FormBuildVM>> ToListAsync()
+        public async Task<Page<FormBuildVM>> PaginateAsync(PageFilter pageInfo)
         {
-            IEnumerable<FormBuild> formBuilds = await _dbContext.FormBuilds.ToListAsync();
-            return Mapper.Map<IEnumerable<FormBuildVM>>(formBuilds);
+            int skip = pageInfo.Size * (pageInfo.Number - 1);
+
+            int total = await _dbContext.FormBuilds.CountAsync();
+            var forms = await _dbContext.FormBuilds.Skip(skip).Take(pageInfo.Size).ToListAsync();
+
+            var formsVM = Mapper.Map<IEnumerable<FormBuildVM>>(forms);
+
+            return new Page<FormBuildVM>(formsVM, total);
         }
     }
 }
