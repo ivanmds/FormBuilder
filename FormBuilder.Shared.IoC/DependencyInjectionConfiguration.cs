e3c591322f55bfef77a68fb80b43225dd1d85104
DESKTOP-S47O7JN\ivanmds <ivanmds2@hotmail.com>
2019-05-06 21:37:07 -0300

    configuração da IoC

---
+++ FormBuilder.Shared.IoC/DependencyInjectionConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using FormBuilder.Core.Data.DbContexts;
+using FormBuilder.Core.Data.Repositories.Forms;
+using FormBuilder.Core.Domain.Interfaces.Repositories.Forms;
+using FormBuilder.Core.Domain.Interfaces.Services.Forms;
+using FormBuilder.Core.Domain.Services.Forms;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FormBuilder.Shared.IoC
+{
+    public static class DependencyInjectionConfiguration
+    {
+        public static void AddDependencyInjection(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(DbContextCore));
+            
+            services.AddTransient<IFormBuildService, FormBuildService>();
+            services.AddTransient<IFormResponseService, FormResponseService>();
+
+            services.AddTransient<IFormBuildRepository, FormBuildRepository>();
+            services.AddTransient<IFormResponseRepository, FormResponseRepository>();
+        }
+    }
+}
