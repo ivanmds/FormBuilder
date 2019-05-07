6e1c14b277d53651e35eb9388fd907712936bbc8
DESKTOP-S47O7JN\ivanmds <ivanmds2@hotmail.com>
2019-05-07 01:04:53 -0300

    trocando por paginacao

--- FormBuilder.Core.Application/AppService/Interfaces/IFormBuildQueryAppService.cs
+++ FormBuilder.Core.Application/AppService/Interfaces/IFormBuildQueryAppService.cs
@@ -1,11 +1,11 @@
 ﻿using System.Threading.Tasks;
-using System.Collections.Generic;
+using FormBuilder.Shared.Kernel.Pagination;
 using FormBuilder.Core.Application.ViewModels.Forms.Builder;
 
 namespace FormBuilder.Core.Application.AppService.Interfaces
 {
     public interface IFormBuildQueryAppService
     {
-        Task<IEnumerable<FormBuildVM>> ToListAsync();
+        Task<Page<FormBuildVM>> PaginateAsync(PageFilter pageInfo);
     }
 }
