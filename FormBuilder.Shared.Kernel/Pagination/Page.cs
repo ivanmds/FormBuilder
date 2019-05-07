e4c11222261166417b970ccaca6210c6e177e657
DESKTOP-S47O7JN\ivanmds <ivanmds2@hotmail.com>
2019-05-06 23:17:36 -0300

    criação da pagina

---
+++ FormBuilder.Shared.Kernel/Pagination/Page.cs
@@ -0,0 +1,17 @@
+﻿using FormBuilder.Shared.Kernel.Model;
+using System.Collections.Generic;
+
+namespace FormBuilder.Shared.Kernel.Pagination
+{
+    public class Page<TEntity> where TEntity : EntityBase
+    {
+        public Page(IEnumerable<TEntity> data, int total)
+        {
+            Data = data;
+            Total = total;
+        }
+
+        public IEnumerable<TEntity> Data { get; protected set; }
+        public int Total { get; protected set; }
+    }
+}
